e7f77aae9a1d198ba67c9b24da8ab4f1d038ac8a
SayMyName302 <awaismalik066@gmail.com>
2022-11-22 19:42:18 +0500

    day6completed

--- lib/Pages/LoginPage.dart
+++ lib/Pages/LoginPage.dart
@@ -9,94 +9,120 @@ class LoginPage extends StatefulWidget {
 class _LoginPageState extends State<LoginPage> {
   String name = "";
   bool changedButton = false;
+  final _fromkey = GlobalKey<FormState>();
+
+  movetohome(BuildContext context) async {
+    if (_fromkey.currentState!.validate()) {
+      setState(() {
+        changedButton = true;
+      });
+
+      await Future.delayed(Duration(seconds: 1));
+      await Navigator.pushNamed(context, MyRoutes.HomeRoute);
+      setState(() {
+        changedButton = false;
+      });
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Material(
         color: Colors.white,
         child: SingleChildScrollView(
-          child: Column(
-            children: [
-              Image.asset(
-                "assests/images/login_image.png",
-                fit: BoxFit.cover,
-              ),
-              SizedBox(
-                height: 20.0,
-              ),
-              Text("Welcome $name",
-                  style: TextStyle(
-                    fontSize: 28,
-                    fontWeight: FontWeight.bold,
-                  )),
-              SizedBox(
-                height: 20.0,
-              ),
-              Padding(
-                padding:
-                    const EdgeInsets.symmetric(vertical: 16, horizontal: 32),
-                child: Column(
-                  children: [
-                    TextFormField(
-                      decoration: InputDecoration(
-                          hintText: "Enter Username", labelText: "username"),
-                      onChanged: (value) {
-                        name = value;
-                        setState(() {});
-                      },
-                    ),
-                    TextFormField(
-                      obscureText: true,
-                      decoration: InputDecoration(
-                          hintText: "Enter Password", labelText: "password"),
-                    ),
-                    SizedBox(
-                      height: 20.0,
-                    ),
-                    InkWell(
-                        onTap: () async {
-                          setState(() {
-                            changedButton = true;
-                          });
-
-                          await Future.delayed(Duration(seconds: 1));
-                          Navigator.pushNamed(context, MyRoutes.HomeRoute);
-                        },
-                        child: AnimatedContainer(
-                          duration: Duration(seconds: 1),
-                          width: changedButton ? 50 : 150,
-                          height: 40,
-                          alignment: Alignment.center,
-                          child: changedButton
-                              ? Icon(
-                                  Icons.done,
-                                  color: Colors.white,
-                                )
-                              : Text("LOGIN",
-                                  style: TextStyle(
-                                      color: Colors.white,
-                                      fontWeight: FontWeight.bold,
-                                      fontSize: 18)),
-                          decoration: BoxDecoration(
-                            color: Colors.red,
-                            //  shape: changedButton?BoxShape.circle:BoxShape.rectangle,
-                            borderRadius:
-                                BorderRadius.circular(changedButton ? 50 : 8),
-                          ),
-                        )),
-                    /*
-                    ElevatedButton(
-                        onPressed: () {
-                          Navigator.pushNamed(context, MyRoutes.HomeRoute);
-                        },
-                        child: Text("LOGIN"),
-                        style:
-                            TextButton.styleFrom(minimumSize: Size(120, 40))),
-                            */
-                  ],
+          child: Form(
+            key: _fromkey,
+            child: Column(
+              children: [
+                Image.asset(
+                  "assests/images/login_image.png",
+                  fit: BoxFit.cover,
                 ),
-              )
-            ],
+                SizedBox(
+                  height: 20.0,
+                ),
+                Text("Welcome $name",
+                    style: TextStyle(
+                      fontSize: 28,
+                      fontWeight: FontWeight.bold,
+                    )),
+                SizedBox(
+                  height: 20.0,
+                ),
+                Padding(
+                  padding:
+                      const EdgeInsets.symmetric(vertical: 16, horizontal: 32),
+                  child: Column(
+                    children: [
+                      TextFormField(
+                        decoration: InputDecoration(
+                            hintText: "Enter Username", labelText: "username"),
+                        validator: (value) {
+                          if (value!.isEmpty) {
+                            return "Username cannot be empty";
+                          } else if (value.length < 5) {
+                            return "Username should be 4 character";
+                          }
+                          return null;
+                        },
+                        onChanged: (value) {
+                          name = value;
+                          setState(() {});
+                        },
+                      ),
+                      TextFormField(
+                        obscureText: true,
+                        decoration: InputDecoration(
+                            hintText: "Enter Password", labelText: "password"),
+                        validator: (value) {
+                          if (value!.isEmpty) {
+                            return "Username cannot be empty";
+                          } else if (value.length < 5) {
+                            return "Password should be 6 character";
+                          }
+                          return null;
+                        },
+                      ),
+                      SizedBox(
+                        height: 20.0,
+                      ),
+                      Material(
+                        color: Colors.red,
+                        borderRadius:
+                            BorderRadius.circular(changedButton ? 50 : 8),
+                        child: InkWell(
+                            onTap: () => movetohome(context),
+                            child: AnimatedContainer(
+                              duration: Duration(seconds: 1),
+                              width: changedButton ? 50 : 150,
+                              height: 40,
+                              alignment: Alignment.center,
+                              child: changedButton
+                                  ? Icon(
+                                      Icons.done,
+                                      color: Colors.white,
+                                    )
+                                  : Text("LOGIN",
+                                      style: TextStyle(
+                                          color: Colors.white,
+                                          fontWeight: FontWeight.bold,
+                                          fontSize: 18)),
+                            )),
+                      ),
+                      /*
+                      ElevatedButton(
+                          onPressed: () {
+                            Navigator.pushNamed(context, MyRoutes.HomeRoute);
+                          },
+                          child: Text("LOGIN"),
+                          style:
+                              TextButton.styleFrom(minimumSize: Size(120, 40))),
+                              */
+                    ],
+                  ),
+                )
+              ],
+            ),
           ),
         ));
   }
